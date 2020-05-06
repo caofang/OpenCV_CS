df6db704eb7705146d49ef5ce4dc0a1923a3b807
FANG CAO <fc63@njit.edu>
2020-05-06 10:06:27 -0400

    opencv c#

---
+++ EMGU.CV/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Mime;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+
+namespace Emgu_CV_Example
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ImageViewer viewer = new ImageViewer();
+
+			var capture = new Capture(0);
+
+			var width = capture.Width;
+			var height = capture.Height;
+
+			var codec = VideoWriter.Fourcc('M', 'J', 'P', 'G');
+			var output = new VideoWriter("myVideo.avi", codec, 60, new Size(width, height), true);
+
+
+			// When the movie playback reaches end, Mat.data becomes NULL.
+			Application.Idle += new EventHandler(delegate (object sender, EventArgs e)
+			{  
+				viewer.Image = capture.QueryFrame(); //draw the image obtained from camera
+
+				var image = capture.QueryFrame();
+
+				//output.Write(image);
+				viewer.Image = image;
+
+			});
+			viewer.ShowDialog();
+			//while (true)
+			//{
+			//	var image = capture.QueryFrame();
+			//	if (image.IsEmpty)
+			//		break;
+
+			//	output.Write(image);
+			//	viewer.Image = image;				
+
+			//	if ((CvInvoke.WaitKey(1) & 0xFF) == 'q')
+			//	{
+			//		break;
+			//	}
+			//}
+
+			capture.Dispose();
+			output.Dispose();
+			
+		}
+	}
+}
---
+++ OpenCVSharp.ImageConversion/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Point = OpenCvSharp.Point;
+using Size = OpenCvSharp.Size;
+
+namespace OpenCVSharp.ImageConversion
+{
+	class Program
+	{
+		private static void ShowImage(string src)
+		{
+			using (var img = new Mat(src, ImreadModes.Grayscale))
+			{
+				using (var window = new Window("window", image: img, flags: WindowMode.AutoSize))
+				{
+					Cv2.WaitKey();
+				}
+			}
+		}
+
+		private static void ShowImage_BitMap(string src)
+		{
+			using (var img = new Mat(src, ImreadModes.Grayscale))
+			{
+				var img_bitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(img);
+				var img_mat = OpenCvSharp.Extensions.BitmapConverter.ToMat(img_bitmap);
+
+				using (var window = new Window("window"))
+				{
+					window.Image = img_mat;
+					Cv2.WaitKey();
+				}
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			var img_src = @"C:\Users\Owner\Desktop\icons\OpenCV_CS\photo.jpg";
+
+			//ShowImage(img_src);
+			ShowImage_BitMap(img_src);
+		}
+	}
+}
